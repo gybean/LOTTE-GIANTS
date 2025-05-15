a03f8c5b1fb5689fca590f32c9f98b2afb43a980
gybean <kimyb0063@naver.com>
2025-05-15 17:48:50 +0900

    롯데자이언츠 리뉴얼

--- js/main.js
+++ js/main.js
@@ -7,4 +7,39 @@ $(function(){
     $('.gnb>li').mouseleave(function(){
         $(this).find('.depth-02').stop().slideUp(300);
     });
+
+
+
+    //section-02
+    //스와이퍼 슬라이드 per view
+    var swiper = new Swiper(".section02Swiper", {
+      slidesPerView: 5,
+      slidesPerGroup: 4,
+      spaceBetween: 12,
+      speed: 700,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      on: {
+        init: function(){
+          updateCustomPagination(this);
+        },
+        slideChange: function(){
+          updateCustomPagination(this);
+        }
+      },
+    });
+
+    function updateCustomPagination(swiperInstance) {
+      const totalPages = Math.ceil(swiperInstance.slides.length / swiperInstance.params.slidesPerGroup) - 1;
+      const currentPage = Math.floor(swiperInstance.activeIndex / swiperInstance.params.slidesPerGroup) + 1;
+
+      // jQuery로 페이지 텍스트 업데이트
+      $('.current-page').text(currentPage);
+      $('.total-pages').text(totalPages);
+    };
+
+
+
 });
