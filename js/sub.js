af8f7d585587342778b1d430c6b58fe9ed4db813
gybean <kimyb0063@naver.com>
2025-06-03 14:25:00 +0900

    최종

--- js/sub.js
+++ js/sub.js
@@ -1,3 +1,15 @@
-$(function(){
-    $('.header').load('../common/header.html')
+$(function () {
+    const textElements = gsap.utils.toArray('.section-02 .b_txt');
+    textElements.forEach(text => {
+        gsap.to(text, {
+            backgroundSize: '100%',
+            ease: 'none',
+            scrollTrigger: {
+                trigger: text,
+                start: 'center 90%',
+                end: 'center 30%',
+                scrub: true,
+            },
+        });
+    });
 });
